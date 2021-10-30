b4f2f4eb932513b5d207d5c07f5661abd5886a39
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-30 19:37:04 +0200

    Creating method to delete row with id

--- Interfaces/methods.go
+++ Interfaces/methods.go
@@ -145,3 +145,19 @@ func Find(u interface{}, id string) []interface{}{
     }
     return data
 }
+
+func Remove(u interface{}, id string) string{
+	db := cfg.Conn
+    name := Name(u)
+    {
+	    findQuery := "SELECT * FROM " + name + " WHERE " + "id = '"+id+"';"
+	    colorstring.Println("[blue]" + findQuery)
+		_, err := db.Query(findQuery)
+		if err != nil{
+			return "NOT FOUND"
+		}
+	}
+	query := "DELETE FROM " + name + " WHERE id = '" + id + "';"
+	db.Exec(query)
+	return "Deleted"
+}	
